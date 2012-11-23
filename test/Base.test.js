1e7e098d57849be9bb67092ca289638d4c42299c
Sean Fridman <fridman@mail.sfsu.edu>
2012-11-23 03:35:24 -0800

    Added a test and tweaked the API

--- test/Base.test.js
+++ test/Base.test.js
@@ -99,6 +99,32 @@ module.exports = exports = {
 
   },
 
+  extendCreateTests: {
+
+    testExtendAndCreate: function(test) {
+      var A = Base.extend({
+        initialize: function(answer) {
+          this.answer = answer;
+        }
+      });
+      var B = A.extendAndCreate({
+        isItSoCool: function() {
+          return this.answer;
+        }
+      }, ['yes!']);
+      test.ok(B.isItSoCool, 'B should be extended with new method');
+      test.ok(B.answer, 'B should have been initialized with args');
+      test.equal(B.isItSoCool(), 'yes!', 'B should be so cool');
+      test.done();
+    },
+
+    testExtendWithDescriptorsAndCreate: function(test) {
+      // TODO
+      test.done();
+    },
+
+  },
+
   createTests: {
 
     testCreate: function(test) {
