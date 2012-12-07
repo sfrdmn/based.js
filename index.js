df7196116fbb27c5df938fe835f660a0f5968138
Sean Fridman <fridman@mail.sfsu.edu>
2012-12-07 00:28:34 -0800

    More decorating...

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var config = require('./config');
 
 module.exports = {
+
   Util: require('./lib/Util'),
 
   Proto: require('./lib/Proto'),
 
   Base: require('./lib/Base'),
+
+  decorators: require('./lib/Decorators'),
 
   configure: function(options) {
     this.Util.mixinKeys(config, options);
